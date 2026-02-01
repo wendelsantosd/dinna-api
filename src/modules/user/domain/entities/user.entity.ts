2800cb2f9547fcb1dbaed100bfb8ecadfe2b5eef
wendelsantosd <wendelwcsantos@gmail.com>
2026-01-31 21:16:02 -0300

    feat: add role to user

--- src/modules/user/domain/entities/user.entity.ts
+++ src/modules/user/domain/entities/user.entity.ts
@@ -1,9 +1,15 @@
+enum Role {
+  ADMIN,
+  USER,
+}
+
 export class User {
   constructor(
     public readonly name: string,
     public readonly email: string,
     public readonly password: string,
     public readonly id?: string,
+    public readonly role?: Role,
     public readonly createdAt?: Date,
     public readonly updatedAt?: Date,
   ) {}
