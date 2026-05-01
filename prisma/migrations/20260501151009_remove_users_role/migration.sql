39d932622e9be1cd65d111e1dd6a732b6105ea25
wendelsantosd <wendelwcsantos@gmail.com>
2026-05-01 12:13:43 -0300

    refactor(user): remove role property

---
+++ prisma/migrations/20260501151009_remove_users_role/migration.sql
@@ -0,0 +1,11 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `role` on the `users` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "users" DROP COLUMN "role";
+
+-- DropEnum
+DROP TYPE "Role";
